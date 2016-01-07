4add0809d404cc050929558303fc3f003329f223
jamesnguyenvnn <james.nguyen.vnn@gmail.com>
2016-01-07 15:55:48 +0700

    add comment for feature #4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::API
   include ActionController::ImplicitRender
 
   def index
+    p 'feature/#4'
     render json:{ message: "I'm Binh Nguyen. My email is : james.nguyen.vnn@gmail.com" }
   end
 end
